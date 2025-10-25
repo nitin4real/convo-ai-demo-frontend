4be28fb2f79b66a541540fe2d5ed7e9f7509b219
nitin <nitin4real@gmail.com>
2025-10-25 10:46:48 +0530

    build fixes

--- src/components/SIP_Agent_Outbound.tsx
+++ src/components/SIP_Agent_Outbound.tsx
@@ -12,11 +12,9 @@ import { AgentTile, IMessage } from '../types/agent.types';
 import { MainCardHeader } from './MainCardHeader';
 import {  FeedbackDialogRef } from './FeedbackDialog';
 import Header from './Header';
-import { PlatformUsageDialogRef } from './PlatformUsageDialog';
 import { Card, CardContent} from './ui/card';
 import { TranscriptionList } from './TranscriptionList';
 import AgoraRTMService from '../services/agora.rtm.services';
-import { IProperties } from './CustomAgent/CustomAgent';
 import { SipOutboundControls } from './SipOutboundControls';
 
 
