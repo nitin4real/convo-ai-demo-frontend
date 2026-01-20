b2448c4362933762da396d95cff1cdf11e4adf28
nitin <nitin4real@gmail.com>
2026-01-20 16:10:25 +0530

    dev: show metric for all the agents

--- src/components/AgentControls.tsx
+++ src/components/AgentControls.tsx
@@ -124,7 +124,7 @@ export const AgentControls = (props: IAgentControlProps) => {
                             >
                                 {showTranscriptions ? <CaptionsOff className="h-6 w-6" /> : <Captions className="h-6 w-6" />}
                             </Button>
-                            {enableMetric && <Button
+                            {true && <Button
                                 title="Metrics"
                                 onClick={() => setShowMetrics(!showMetrics)}
                                 variant={showMetrics ? "destructive" : "outline"}
